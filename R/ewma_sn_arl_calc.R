1badc70a52d5cdbe13c8bdbe890e11fc0439214b
marcelofonsecab <66529064+marcelofonsecab@users.noreply.github.com>
2023-07-08 11:58:21 -0300

    Add files via upload

--- R/ewma_sn_arl_calc.R
+++ R/ewma_sn_arl_calc.R
@@ -1,37 +1,58 @@
-ewma_sn_arl_calc =
-  function(lambda = 0.1,
-           L = 2.667,
-           n = 1,
-           sim = 10000) {
-    
-    ucl = L * sqrt(lambda * n * (1 - (1 - lambda) ^ 2) / (2 - lambda))
-    lcl = - ucl
-    
-    runlength = numeric(sim)
-    xi = matrix(rnorm(sim * n, mean, 1), nrow = n)
-    ti = rowSums(xi >= 0)
-    sni = 2 * ti - n
-    
-    zi = numeric(sim)
-    indicator = numeric(sim)
-    
-    for (j in 1:sim) {
-      if (j == 1) {
-        zi[j] <- lambda * sni[j]
-      } else {
-        zi[j] <- lambda * sni[j] + (1 - lambda) * zi[j - 1]
-      }
-      
-      if (zi[j] > UCL_steady | zi[j] < LCL_steady) {
-        indicator[j] <- 1
-      }
-      
-      if (any(indicator)) {
-        runlength[j] <- which.max(indicator == 1)
-      } else {
-        runlength[j] <- sim
-      }
+#' Average Run Length (ARL) Calculation for EWMA Control Chart
+#'
+#' Calculates the average run length (ARL) for the Exponentially Weighted Moving Average (EWMA) control chart.
+#'
+#' @param lambda The decay factor for the EWMA. Default is 0.1.
+#' @param L The multiplier for the control limits. Default is 2.667.
+#' @param n The number of observations in each simulation. Default is 1.
+#' @param sim The number of simulations to perform. Default is 10000.
+#'
+#' @return A numeric vector representing the average run length (ARL) for each simulation.
+#'
+#' @details The function calculates the average run length (ARL) for the EWMA control chart.
+#'   It performs multiple simulations based on the specified parameters and returns the ARL for each simulation.
+#'   The control limits are calculated based on the lambda and L parameters.
+#'   The ARL is the number of observations until the control chart signals an out-of-control condition.
+#'
+#' @examples
+#' # Calculate ARL for EWMA control chart
+#' ewma_sn_arl_calc(lambda = 0.1, L = 2.667, n = 1, sim = 10000)
+#'
+#' @importFrom stats rnorm rowSums
+#' @export
+ewma_sn_arl_calc <- function(lambda = 0.1,
+                             L = 2.667,
+                             n = 1,
+                             sim = 10000) {
+
+  ucl = L * sqrt(lambda * n * (1 - (1 - lambda) ^ 2) / (2 - lambda))
+  lcl = -ucl
+
+  runlength = numeric(sim)
+  xi = matrix(rnorm(sim * n, mean, 1), nrow = n)
+  ti = rowSums(xi >= 0)
+  sni = 2 * ti - n
+
+  zi = numeric(sim)
+  indicator = numeric(sim)
+
+  for (j in 1:sim) {
+    if (j == 1) {
+      zi[j] <- lambda * sni[j]
+    } else {
+      zi[j] <- lambda * sni[j] + (1 - lambda) * zi[j - 1]
     }
-    
-    return(runlength)
+
+    if (zi[j] > UCL_steady | zi[j] < LCL_steady) {
+      indicator[j] <- 1
+    }
+
+    if (any(indicator)) {
+      runlength[j] <- which.max(indicator == 1)
+    } else {
+      runlength[j] <- sim
+    }
   }
+
+  return(runlength)
+}
--- R/shewhart_mw.R
+++ R/shewhart_mw.R
@@ -1,52 +1,63 @@
-#shewhart_mw
+#' Shewhart Control Chart based on Mann-Whitney Statistic
+#'
+#' Calculates and plots the Shewhart Control Chart based on the Mann-Whitney statistic.
+#'
+#' @param X A numeric matrix or data frame containing the data.
+#' @param far The desired false alarm rate for the control chart. Default is 0.0027.
+#' @param group_by_col A logical value indicating whether to treat columns as groups.
+#'   If TRUE, the matrix will be transposed. Default is FALSE.
+#' @param phase1_num_samples The number of samples to include in phase 1. Default is NULL.
+#' @param side The side of the chart to plot. Must be one of 'two.sided', 'lower', or 'upper'.
+#'   Default is 'two.sided'.
+#'
+#' @details The function calculates the Shewhart Control Chart based on the Mann-Whitney statistic.
+#'   It divides the data into two phases: phase 1 and phase 2.
+#'   Phase 1 consists of the specified number of samples, and phase 2 consists of the remaining samples.
+#'   The Mann-Whitney statistic is computed between each sample in phase 2 and the phase 1 samples.
+#'   The control limits are calculated based on the desired false alarm rate (far).
+#'   The control chart is then plotted using the plot_chart function.
+#'
+#' @seealso \code{\link{plot_chart}}
+#'
+#' @examples
+#' # Generate example data
+#' set.seed(42)
+#' X <- matrix(rnorm(100), nrow = 20)
+#'
+#' # Calculate Shewhart Control Chart based on Mann-Whitney statistic
+#' shewhart_mw(X, far = 0.0027, group_by_col = FALSE, phase1_num_samples = 2, side = 'two.sided')
+#'
+#' @importFrom graphics plot
+#' @importFrom stats wilcox.test
+#' @importFrom base warning stop
+#' @export
+shewhart_mw <- function(X, far = 0.0027, group_by_col = FALSE,
+                        phase1_num_samples = NULL,
+                        side = "two.sided") {
 
-X = matrix(rnorm(100), nrow = 20) |> data.frame()
-
-phase1 = X[, 1:2]
-phase2 = X[, -seq(1:2)]
-
-phase1 = phase1 |> unlist()
-
-sapply(phase2, function(x) wilcox.test(phase1, x)$statistic)
-
-
-
-shewhart_mw = function(X, far = .0027, group_by_col = FALSE,
-                       phase1_num_samples = NULL,
-                       side = "two.sided"){
-  
   X = X |> data.frame()
-  
+
   if (group_by_col) {
     X = t(X)
   }
-  
-  if(is.null(phase1_num_samples)){
-    #por enquanto vai receber 1
-    #considerar uma função para calcular tamanho de amostra depois
+
+  if (is.null(phase1_num_samples)) {
     phase1_num_samples = 1
   }
-  
-  #phase1 and 2 samples
-  phase1 = X[,1:phase1_num_samples] |> unlist()
+
+  phase1 = X[, 1:phase1_num_samples] |> unlist()
   phase2 = X[, -seq(1:phase1_num_samples)]
-  
+
   if (any(unlist(phase1) == unlist(phase2)) | any(is.na(X))) {
     warning("There is a tie or NA in your data. Caution with your results.")
   }
-  
-  #computing Mann Whitney statistitcs 
+
   mw_statistics = sapply(phase2, function(x) wilcox.test(phase1, x)$statistic)
-  
-  #computing ucl
-  
-  
-  #computing LCL (mn_i - ucl)
-  lcl = length(phase1)*sapply(phase2, function(x) sum(!is.na(x))) - ucl
-  
-  
+
+  ucl = length(phase1) * sapply(phase2, function(x) sum(!is.na(x))) - ucl
+
   if (side == "two.sided") {
-    a = 2*quantil - nrow(X)
+    a = 2 * quantil - nrow(X)
     la = -a
   } else if (side == "lower") {
     a = quantil
@@ -55,14 +66,12 @@ shewhart_mw = function(X, far = .0027, group_by_col = FALSE,
     a = NULL
     la = -quantil
   } else {
-    stop("Invalid argument for side. Must be one of 'two.sided',
-         'lower', or 'upper'")
+    stop("Invalid argument for side. Must be one of 'two.sided', 'lower', or 'upper'")
   }
-  
-  arl = round(1/far)
-  
+
+  arl = round(1 / far)
+
   plot_chart(statistics = mw_statistics, ic = median(mw_statistics),
              ucl = ucl, lcl = lcl,
-             name = "Shewhart MW (Based on Mann Whitney Statistic)")
-  
+             name = "Shewhart MW (Based on Mann-Whitney Statistic)")
 }
